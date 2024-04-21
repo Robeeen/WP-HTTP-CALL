bed7a7d4d4774f802822745fa3c29ef47e4a72bd
Robeeen <robeeen@gmail.com>
2024-04-21 19:36:33 +0600

    Update from local

---
+++ http_call.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WP HTTP CALL
+Plugin URI: https://shamskhan.xyz
+Description: simple plugin that retrieves data from a remote API endpoint
+Author: Shams Khan
+Version: 1.0.0
+Author URI: https://shamskhan.xyz
+*/
+
+if( !defined('ABSPATH'))
+{
+    exit; // Exit if accessed directly
+}
+
