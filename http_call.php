35f307d47e355b7f235417ee7cfd5b5d2b37e3df
Robeeen <robeeen@gmail.com>
2024-04-21 21:33:45 +0600

    udpates

--- http_call.php
+++ http_call.php
@@ -8,18 +8,22 @@ Version: 1.0.0
 Author URI: https://shamskhan.xyz
 */
 
+// Exit if accessed directly
 if( !defined('ABSPATH'))
 {
-    exit; // Exit if accessed directly
+    exit; 
 }
 
 function call_the_api(){
     //calling the api
     $url = "https://api.github.com/users/wordpress";
-    $response = wp_remote_get($url);
-
-    echo $response;
-
+    $response = wp_remote_get(esc_url_raw($url));
+    //parsed from json objects
+    $display_body = json_decode(wp_remote_retrieve_body($response), true);
+    //print_r($display_body);
+ 	foreach($display_body as $key=>$value){
+		echo $key . ":" . $value . "</br>";
+	}
 }
 add_shortcode('http-call', 'call_the_api');
 
