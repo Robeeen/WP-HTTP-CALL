753142034c75f79122de42513fadd5e539fb37cd
Robeeen <robeeen@gmail.com>
2024-04-21 23:08:04 +0600

    udpates

--- http_call.php
+++ http_call.php
@@ -17,7 +17,10 @@ if( !defined('ABSPATH'))
 function call_the_api(){
     //calling the api
     $url = "https://api.github.com/users/wordpress";
-    $response = wp_remote_get(esc_url_raw($url));
+    $argument = array(
+        'method' => 'GET'
+    );
+    $response = wp_remote_get(esc_url_raw($url, $argument));
     //parsed from json objects
     $display_body = json_decode(wp_remote_retrieve_body($response), true);
     //print_r($display_body);
