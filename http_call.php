9985aa7d21144bc1d85f21b4f765a1bf02e3ae43
Robeeen <robeeen@gmail.com>
2024-04-22 22:32:15 +0600

    update

--- http_call.php
+++ http_call.php
@@ -16,17 +16,33 @@ if( !defined('ABSPATH'))
 
 function call_the_api(){
     //calling the api
-    $url = "https://api.github.com/users/wordpress";
+    $url = "https://fakestoreapi.com/products";
     $argument = array(
-        'method' => 'GET'
+        'method' => 'GET',
+        'timeout' => 120
     );
     $response = wp_remote_get(esc_url_raw($url, $argument));
+    if(is_wp_error($response)){
+        echo 'An internal error has occured';
+        return;
+    }
     //parsed from json objects
-    $display_body = json_decode(wp_remote_retrieve_body($response), true);
-    //print_r($display_body);
- 	foreach($display_body as $key=>$value){
-		echo $key . ":" . $value . "</br>";
-	}
+    $data = json_decode(wp_remote_retrieve_body($response), true);
+
+    $code = wp_remote_retrieve_response_code($data);
+
+    if( 200 !== wp_remote_retrieve_response_code($code)){
+        echo 'An https error has occured';
+        return;
+    }
+
+    echo '<div>';
+    foreach($data as $product){
+        echo '<h3>' . $product->title . '</h3>';
+        echo '<p>' . $product->price . '</p>';
+    }
+    echo '</div>';
+ 	
 }
 add_shortcode('http-call', 'call_the_api');
 
